e219a3655b2f8a3c483f0eed361e9b1b0cf958e7
Parker Williamson <parker.williamson@gmail.com>
2020-06-02 21:01:03 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -3,23 +3,30 @@
 def highest_student_gpa
   "SELECT MAX(gpa) 
    FROM students
-   "
+  "
 end
 
 def lowest_student_gpa
   "SELECT MIN(gpa) 
    FROM students
-   "
+  "
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students"
+  "SELECT AVG(gpa) 
+   FROM students
+  "
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students"
+  "SELECT SUM(tardies)
+   FROM students
+  "
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM(students) WHERE grade=9"
+  "SELECT AVG(gpa) 
+   FROM(students) 
+   WHERE grade=9
+  "
 end
